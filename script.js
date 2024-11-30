5e5e22e18a3b3d04a40c9182dddbda30b9380db6
lug1212 <58533425+lug1212@users.noreply.github.com>
2024-11-30 11:49:30 -0800

    update 0.02

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 //variables :
 let money = 0;
 let gain = 1;
+let gainpersecond = 0;
 //functions :
 function onclicked() {
     money += gain
     document.querySelector('.money').innerText = 'Money: ' + money;
 }
+function Aps(){
+    money += gainpersecond;
+    document.querySelector('.money').innerText = 'Money: '+ money;
+}
 
-//event listener :
 // event listener :
 document.getElementById('element').addEventListener('click', function(event) {
     const plusOneContainer = document.createElement('div');
@@ -27,11 +31,26 @@ document.getElementById('element').addEventListener('click', function(event) {
         document.body.removeChild(plusOneContainer);
     }, 2000);
 });
-
+document.getElementById('ejminer').addEventListener('click', function(event) {
+    const cost = 200;
+    if (money >= cost){
+        money -= cost;
+        gainpersecond += 1;
+        document.querySelector('.money').innerText = 'Money: '+ money;
+        document.querySelector('.aps').innerText = 'APS: '+ gainpersecond;
+    }
+    else {
+        return
+    }
+});
 document.getElementById('buyElement').addEventListener('click', event => {
-    if (money >= 25) {
+    const cost = 25
+    
+    if (money >= cost) {
         gain += 1;
-        money -= 25;
+        money -= cost;
+        
+        cost
         document.querySelector('.money').innerText = 'Money: '+ money;
     } else {
         return
@@ -42,3 +61,5 @@ document.getElementById('toggleShop').addEventListener('click', function() {
     shop.style.display = (shop.style.display === 'none' || shop.style.display === '') ? 'block' : 'none';
 });
 document.getElementById('element').addEventListener('click', onclicked);
+//inetverals
+setInterval(Aps,1000);
